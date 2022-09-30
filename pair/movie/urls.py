91524f47348b496e86d774a3da03451167ffbeae
woocheolll <woocheol2868@gmail.com>
2022-09-30 13:02:21 +0900

    pair

--- pair/movie/urls.py
+++ pair/movie/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name= 'movie'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('detail/<int:pk_>', views.detail, name='detail'),
+    path('edit/<int:pk_>', views.edit, name='edit'),
+    path('update/<int:pk_>', views.update, name='update'),
 ]
--- pair/movie/views.py
+++ pair/movie/views.py
@@ -1,9 +1,60 @@
-from django.shortcuts import render
+from django.dispatch import receiver
+from django.shortcuts import render, redirect
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, 'movie/index.html')
+    movie = Movie.objects.all()
+    context = {
+        'movie' : movie,
+    }
+    return render(request, 'movie/index.html', context)
 
 def new(request):
-    return render(request, 'movie/index.html')
+    
+    return render(request, 'movie/new.html')
 
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    
+    Movie.objects.create(title=title, content=content)
+    context = {
+        'title' : title,
+        'content' : content,
+    }
+    return render(request, 'posts/create.html', context)
+
+    # return render(request, 'posts/create.html', context)
+    #return redirect('movie:index')
+
+def delete(request, pk):
+    Movie.objects.get(id=pk).delete()
+    return redirect('movie:index')
+
+def detail(request, pk):
+    movies = Movie.objects.get(pk = pk)
+    context = {
+        'movies' : movies,
+    }
+    return render(request, 'movie/detail.html',context)
+
+def edit(request, pk):
+  movies = Movie.objects.get(pk = pk)
+  context = {
+    "movies": movies,
+  }
+  
+  return render(request, 'movie/edit.html', context)
+    
+def update(request,pk):
+    movies = Movie.objects.get(pk = pk)
+    
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    
+    movies.title = title
+    movies.content = content
+    movies.save()
+       
+    return redirect('movie:detail', movies.pk)
