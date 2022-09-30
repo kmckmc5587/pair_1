fb0110c749b6a91d534e9ba9c8792a66f7ac940f
woocheolll <woocheol2868@gmail.com>
2022-09-30 14:37:34 +0900

    기본 틀 완료

--- pair/movie/urls.py
+++ pair/movie/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
     path('delete/<int:pk>', views.delete, name='delete'),
-    path('detail/<int:pk_>', views.detail, name='detail'),
-    path('edit/<int:pk_>', views.edit, name='edit'),
-    path('update/<int:pk_>', views.update, name='update'),
+    path('detail/<int:pk>', views.detail, name='detail'),
+    path('edit/<int:pk>', views.edit, name='edit'),
+    path('update/<int:pk>', views.update, name='update'),
 ]
